0c4743afdd212d0ba527a8b2f5369b6cd9adc595
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-06-03 09:54:29 +0100

    feat: add preview image to dashboard

--- sanity/schema/comment.schema.ts
+++ sanity/schema/comment.schema.ts
@@ -33,25 +33,33 @@ const comment: any = {
       type: "reference",
       to: [{ type: "post" }],
     },
+    {
+      name: "previewImg",
+      type: "image",
+    },
   ],
   preview: {
     select: {
       name: "name",
       comment: "comment",
       post: "post.title",
+      previewImg: "post.image.asset.url",
     },
     prepare({
       name,
       comment,
       post,
+      previewImg,
     }: {
       name: string;
       comment: string;
       post: { title: string };
+      previewImg: string;
     }) {
       return {
         title: `${name} on ${post}`,
         subtitle: comment,
+        imageUrl: previewImg,
       };
     },
   },
