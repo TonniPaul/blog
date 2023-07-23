19e960527b115d0bbae09b7b32b8ce7abeb0f8c4
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-07-23 21:46:30 +0100

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import Navbar from "@/components/navbar/Navbar";
 import { play } from "@/helpers/fonts";
 import GlobalStyles from "@/styles/app.style";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactElement, ReactNode } from "react";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       <main className={play.className}>
         <GlobalStyles />
         {getLayout(<Component {...pageProps} />)}
+        <Analytics />
       </main>
     </>
   );
