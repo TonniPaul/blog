a371ae3afc67ed4310e9a2ce38519fc95fc92077
TonniPaul <ariyoadeoyepaul@gmail.com>
2024-02-18 23:11:17 +0100

    feat: create constants file and update config to use variables

---
+++ helpers/constants.ts
@@ -0,0 +1,3 @@
+export const SANITY_PRODUCT_ID = process.env.SANITY_PRODUCT_ID as string;
+export const SANITY_DATASET = process.env.SANITY_DATASET as string;
+export const SANITY_TOKEN = process.env.SANITY_TOKEN as string;
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,19 +2,14 @@ import { defineConfig } from "sanity";
 import { visionTool } from "@sanity/vision";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schema";
+import { SANITY_DATASET, SANITY_PRODUCT_ID } from "./helpers/constants";
 
 export default defineConfig({
-  name: "default",
-  title: "BLOG DASHBOARD",
-
-  projectId: "8vq19kgu",
-  dataset: "production",
-
+  title: "Dashboard",
+  projectId: SANITY_PRODUCT_ID,
+  dataset: SANITY_DATASET,
   apiVersion: "2023-05-26",
-
   basePath: "/admin",
-
   plugins: [deskTool(), visionTool()],
-
   schema: { types: schemas },
 });
